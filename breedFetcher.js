f9c352357f8ce93d1d77f55210902f90f7f6a502
mwong01 <mattw90@gmail.com>
2019-10-24 17:36:11 +0000

    finding cat breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,15 @@
-const fs = require('fs');
+let url = "https://api.thecatapi.com/v1/breeds/search?q=" + process.argv[2];
+
+const request = require('request');
+request(url, (error, response, body) => {
+  if (body = []) {
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    console.log("No such cat exists");
+    return;
+  }
+  const data = JSON.parse(body);
+  console.log(data[0].description);
+});
+
+
